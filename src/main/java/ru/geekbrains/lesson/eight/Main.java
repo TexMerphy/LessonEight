5efa39e9d6c4b0163e2fe05ed9d723f31859e824
TexMerph <loshakovpv@yandex.ru>
2021-08-17 18:36:58 +0300

    2 commit

--- src/main/java/ru/geekbrains/lesson/eight/Main.java
+++ src/main/java/ru/geekbrains/lesson/eight/Main.java
@@ -1,4 +1,62 @@
 package ru.geekbrains.lesson.eight;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
+    public static void main(String[] args) {
+        File file = new File("D:\\WorkDir\\LessonEight\\src\\test");
+        if (!file.exists()) {
+            System.out.println("Директория не существует.");
+            if (file.mkdirs()) {
+                System.out.println("Директория успешно создана.");
+            } else {
+                System.out.println("Не удалось создать директорию.");
+            }
+        } else {
+            System.out.println("Директория существует.");
+        }
+
+        try {
+            FileInputStream fileOneStream = new FileInputStream("D:\\WorkDir\\LessonEight\\src\\test\\test_file1.txt");
+            FileInputStream fileTwoStream = new FileInputStream("D:\\WorkDir\\LessonEight\\src\\test\\test_file2.txt");
+            InputStreamReader fileOneRead = new InputStreamReader(fileOneStream, StandardCharsets.UTF_8);
+            InputStreamReader fileTwoRead = new InputStreamReader(fileOneStream, StandardCharsets.UTF_8);
+
+
+            int i = -1;
+
+            while ((i = fileOneStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+            while ((i = fileTwoStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+            /**
+            inputStreamReader.close();
+            fileInputStream01.close();
+            System.out.println("\n*** Чтение файла завершено. ***\n");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            FileOutputStream fileOutputStream01 = new FileOutputStream("C:\\sandbox\\sample_output.txt");
+            byte[] buf = str01.getBytes(StandardCharsets.UTF_8);
+            fileOutputStream01.write(buf, 0, buf.length);
+            fileOutputStream01.close();
+            System.out.println("Данные успешно записаны в файл (1)");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }*/
+    }
 }
